3f5d931005e507dc2c724457949afd370de70bd1
Ismam Ahnaf Mahin <ismam.mahin@northsouth.edu>
2023-04-30 23:54:55 +0600

    Sleep function replaced with delay function as sleep function doesn't work on some pcs

--- main.c
+++ main.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 #include<unistd.h>
 #include<dos.h>
-#include"splashscreen.h"
+#include<time.h>
+#include"splashscreen.h"      //header file containing the function for splashscreen
+#include"delay.h"             //header file containig delay function
 int main(void)
 {
-    splashscreen();
-    sleep(4);
-    system("cls");
+    splashscreen();           //splashscreen that is shown at the start of the program
+    delay(4);                 //the splashscreen will stay for 4 seconds
+    system("cls");            //splashscreen will get cleared
     printf("Hello world!");
     return 0;
 }

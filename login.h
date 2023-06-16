d7a31d5006614e3ba35e6027395c70a0b4fbb53b
Ismam Ahnaf Mahin <ismam.mahin@northsouth.edu>
2023-06-16 16:44:31 +0600

    Login file added to the code.

---
+++ login.h
@@ -0,0 +1,94 @@
+// Making a login screen which has both signup and signin functionality and takes info from files
+#include <stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<conio.h>
+struct login
+{
+    char first_name[100];
+    char last_name[100];
+    char username[100];
+    char password[20];
+};
+
+void signup(void);
+
+void signin(void);
+
+void login(void)
+{
+    int n = 2;
+    do
+    {
+        if (!(n==1||n==2))
+        {
+            printf("Wrong input! Try again\n");
+        }
+
+        printf("Press '1' to signup.\n");
+        printf("Press '2' to sign in.\n");
+        scanf("%d", &n);
+    } while (!(n==1||n==2));
+    system("CLS");
+    if (n == 1)
+    {
+        signup();
+    }
+    else
+    {
+        signin();
+    }
+}
+void signup(void)
+{
+    FILE *log;
+    log = fopen("login.txt", "w");
+    struct login l;
+    printf("Enter your first name: ");
+    scanf("%s", l.first_name);
+    printf("Enter your last name: ");
+    scanf("%s", l.last_name);
+    printf("Enter username: ");
+    scanf("%s", l.username);
+    printf("Enter password: ");
+    scanf("%s", l.password);
+    fwrite(&l, sizeof(l), 1, log);
+    fclose(log);
+    system("CLS");
+    printf("Signup successful!!!");
+    signin();
+}
+void signin(void)
+{
+    char username[100];
+    char password[20];
+    FILE *log;
+    log = fopen("login.txt", "r");
+    struct login l;
+    int s;
+    do
+    {
+        printf("Enter Username: ");
+        scanf("%s", &username);
+        printf("Enter Password: ");
+        scanf("%s", &password);
+        fseek(log, 0, SEEK_SET);
+        while (fread(&l, sizeof(l), 1, log))
+        {
+            if (strcmp(username, l.username) == 0 && strcmp(password, l.password) == 0)
+            {
+                printf("Login successful!\n");
+                printf("Welcome %s %s\n",l.first_name,l.last_name);
+                getch();
+                s = 0;
+            }
+            else
+            {
+                system("CLS");
+                printf("Incorrect ID or password! Please try again\n");
+                s = -1;
+            }
+        }
+    } while (s == -1);
+    fclose(log);
+}
--- main.c
+++ main.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
-#include<unistd.h>
-#include<dos.h>
 #include<time.h>
 #include"splashscreen.h"      //header file containing the function for splashscreen
 #include"delay.h"             //header file containig delay function
+#include"login.h"             //header file containig the function for login;
 int main(void)
 {
     splashscreen();           //splashscreen that is shown at the start of the program
     delay(4);                 //the splashscreen will stay for 4 seconds
     system("cls");            //splashscreen will get cleared
+    login();
     printf("Hello world!");
     return 0;
 }

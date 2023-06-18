4daf3cada17f2940016033b5dc6206398f058e2f
Ismam Ahnaf Mahin <ismam.mahin@northsouth.edu>
2023-06-18 13:55:01 +0600

    Music added during splashscreen

--- main.c
+++ main.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include <time.h>
+#include <windows.h>
 #include "splashscreen.h" //header file containing the function for splashscreen
 #include "delay.h"        //header file containing delay function
 #include "login.h"        //header file containing the function for login
 #include "menu.h"         //header file containing the menu of item and purchasing system
+
 void openImageDeliveryGuy(void);
+void music(void);
+void beep(int, int);
 
 int main(void)
 {
     splashscreen(); // splashscreen that is shown at the start of the program
-    // getch();
-    delay(4);      // the splashscreen will stay for 4 seconds
+    music(); //Adds a music during splashscreen
     system("cls"); // splashscreen will get cleared
     login();       // login process
     system("cls");
@@ -32,4 +35,36 @@ void openImageDeliveryGuy(void)
 #error "Unsupported operating system"
 #endif
     system(command);
+}
+
+void beep(int frequency, int duration) {
+    #ifdef _WIN32
+        // Windows implementation
+        Beep(frequency, duration);
+    #else
+        // Unix-like implementation
+        printf("\033[10;%d]\a", frequency);
+        usleep(duration * 1000);
+        printf("\033[10;0]\a");
+    #endif
+}
+
+void music(void)
+{
+    // Define the melody
+    int melody[] = {
+        659, 783, 880, 987, 880, 783, 659, 587, 523, 523, 523, 659, 783, 880, 987, 880, 783, 659, 698, 659, 587
+    };
+    int duration[] = {
+        250, 250, 250, 250, 250, 250, 250, 250, 250, 250, 250, 250, 250, 250, 250, 250, 250, 250, 250, 500
+    };
+
+    // Calculate the size of the melody array
+    int size = sizeof(melody) / sizeof(melody[0]);
+
+    // Play the melody structure
+    for (int i = 0; i < size; i++) {
+        beep(melody[i], duration[i]);
+        delay(.2); // Pause between notes
+    }
 }
